8b1dedb0ae5c9621e48c70dbcf5b2c98a57d1326
Kevin Chuang <kcxinkun@gmail.com>
2015-12-24 23:07:12 -0800

    add vote tracker

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+
 // Create array and push kitten contestants inside. Awwwwwwwwww so cute.
 
 this.kittenVote = [];
+$(document).ready(function(){
 
-var contestants = function kittens(id,name,pic) {
+var contestants = function kittens(id,name,pic,votes) {
   this.id = id;
   this.name = name;
   this.pic = pic;
+  this.votes = votes
 }
 
-var kOne = new contestants(1,"Bella","img/01.jpg")
-var kTwo = new contestants(2,"Kuro","img/02.jpg")
-var kThree = new contestants(3,"Tibby","img/03.jpg")
-var kFour = new contestants(4,"Shoestring","img/04.jpg")
-var kFive = new contestants(5,"Ginny","img/05.jpg")
-var kSix = new contestants(6,"The Rosie Family","img/06.jpg")
-var kSeven = new contestants(7,"Boots","img/07.jpg")
-var kEight = new contestants(8,"Tiggy","img/08.jpg")
-var kNine = new contestants(9,"Sheets","img/09.jpg")
-var kTen = new contestants(10,"Spidercat","img/10.jpg")
-var kEleven = new contestants(11,"OmNomNom","img/11.jpg")
-var kTwelve = new contestants(12,"Sunny","img/12.jpg")
-var kThirteen = new contestants(13,"Nibbler","img/13.jpg")
-var kFourteen = new contestants(14,"Soul Crusher von Rektienschnein the IX","img/14.jpg")
+var kOne = new contestants(1,"Bella","img/01.jpg",0)
+var kTwo = new contestants(2,"Kuro","img/02.jpg",0)
+var kThree = new contestants(3,"Tibby","img/03.jpg",0)
+var kFour = new contestants(4,"Shoestring","img/04.jpg",0)
+var kFive = new contestants(5,"Ginny","img/05.jpg",0)
+var kSix = new contestants(6,"The Rosie Family","img/06.jpg",0)
+var kSeven = new contestants(7,"Boots","img/07.jpg",0)
+var kEight = new contestants(8,"Tiggy","img/08.jpg",0)
+var kNine = new contestants(9,"Sheets","img/09.jpg",0)
+var kTen = new contestants(10,"Spidercat","img/10.jpg",0)
+var kEleven = new contestants(11,"OmNomNom","img/11.jpg",0)
+var kTwelve = new contestants(12,"Sunny","img/12.jpg",0)
+var kThirteen = new contestants(13,"Nibbler","img/13.jpg",0)
+var kFourteen = new contestants(14,"Soul Crusher von Rektienschnein the IX","img/14.jpg",0)
 
 kittenVote.push(kOne);
 kittenVote.push(kTwo);
@@ -46,6 +49,8 @@ var kittyRandom = function randomCat() {
 
 var $kittenOne = $('#kittenOne');
 var $kittenTwo = $('#kittenTwo');
+var $nameOne = $('#nameOne');
+var $nameTwo = $('#nameTwo');
 var rdmIntOne = kittyRandom();
 var rdmIntTwo = kittyRandom();
 console.log(rdmIntOne);
@@ -54,24 +59,43 @@ console.log(rdmIntTwo);
 var firstKitten = kittenVote.forEach(function (cat) {
   if (rdmIntOne === cat.id) {
     $kittenOne.html("<img src=" + cat.pic+"></img>");
-    console.log(rdmIntOne);
+    $nameOne.html(cat.name);
+    //console.log(rdmIntOne);
   };
 });
 
 var secondKitten = kittenVote.forEach(function (cat) {
-  while (rdmIntTwo === rdmIntOne) {
+  while (rdmIntTwo === rdmIntOne) { //reroll if kittens chosen are the same
     rdmIntTwo = kittyRandom();
   };
 
   if (rdmIntTwo === cat.id) {
     $kittenTwo.html("<img src=" + cat.pic + "></img>");
-    console.log(rdmIntTwo);
+    $nameTwo.html(cat.name);
+    //console.log(rdmIntTwo);
   }
 });
-console.log(rdmIntTwo);
+//console.log(rdmIntTwo);
+
+// Button voting
+
+var $kittenOneBtn = $('#kittenOneBtn');
+var $kittenTwoBtn = $('#kittenTwoBtn');
+
+$kittenOneBtn.click(function() {
+  kittenVote[rdmIntOne].votes += 1;
+  console.log(kittenVote[rdmIntOne].votes);
+})
+
+$kittenTwoBtn.click(function() {
+  kittenVote[rdmIntTwo].votes += 1;
+  console.log(kittenVote[rdmIntTwo].votes);
+})
+
+// Display results on a chart
 
 // Prompt user to vote on two more kittens
 
 // Store voting results in localStorage
 
-// Display results on a chart
+});
